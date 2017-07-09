7b8ed4364cc3ee8b811353fce448f1bec104dc5f
Hoang Hiep Nguyen <hoanghiepnguyen106@gmail.com>
2017-07-09 17:58:45 +0700

    Add formatSeconds and tests for it

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ module.exports = {
             Nav: 'src/components/Nav.jsx',
             Timer: 'src/components/Timer.jsx',
             Countdown: 'src/components/Countdown.jsx',
+            Clock: 'src/components/Clock.jsx',
             applicationStyle: 'src/styles/app.scss'
         },
         extensions: ['', '.js', '.jsx']
